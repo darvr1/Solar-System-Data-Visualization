400cba5ebe9fddbc26363127a0c2179df27a5821
student <dalegeethoob@gmail.com>
2023-11-26 21:30:33 -0800

    implemented hide button for navigation

--- public/script.js
+++ public/script.js
@@ -1,27 +1,60 @@
 { // Navigation
 // Planet dropdown
-let box = document.querySelector("nav div");
-let isOpen = false;
+const planetDropdownBox = document.querySelector("nav div");
+const nav = document.querySelector("nav");
+const navButton = document.getElementById("stickButton");
+let isDropdownOpen = false;
+let isNavOpen = true;
 
 // Alternate between opening and closing dropdown
 function planetDropdown() {
-    isOpen = !isOpen;
-    if (isOpen) {
-        box.style.height = "256px";
+    isDropdownOpen = !isDropdownOpen;
+    if (isDropdownOpen) {
+        setTimeout(() => {
+            planetDropdownBox.style.overflowY = "scroll";
+        }, 500);
+        planetDropdownBox.style.height = "256px";
     } else {
-        box.style.height = "0px";
+        planetDropdownBox.style.overflowY = "hidden";
+        planetDropdownBox.style.height = "0px";
     }
-    sessionStorage.setItem("buttonState", isOpen);
+    sessionStorage.setItem("planetButtonState", isDropdownOpen);
 }
 
-// If the dropdown is left open, keep it open if the page changes
+// Open and close navigation bar
+function navSwitch() {
+    isNavOpen = !isNavOpen;
+    if (isNavOpen) {
+        navButton.style.right = "140px";
+        nav.style.width = "135px";
+        // Re-apply initial overflow propert when transition is finished
+        setTimeout(() => {
+            nav.style.overflow = "scroll";
+        }, 500);
+        setTimeout(() => {
+            navButton.textContent = "→";
+        }, 250);
+    } else {
+        navButton.style.right = "5px";
+        nav.style.width = "0px";
+        // Hide scrollbar during transition
+        nav.style.overflow = "hidden";
+        setTimeout(() => {
+            navButton.textContent = "←";
+        }, 250);
+    }
+}
+
+// If the dropdown and/or nav is left open, keep it open if the page changes
 window.addEventListener("load", () => {
     // Need to come up with a better name
-    const PLACEHOLDERNAME = sessionStorage.getItem("buttonState");
-    box.style.transition = "none";
+    const PLACEHOLDERNAME = sessionStorage.getItem("planetButtonState");
+    planetDropdownBox.style.transition = "none";
     if (PLACEHOLDERNAME == "true") {
         planetDropdown();
     }
-    window.setTimeout(() => {box.style.transition = "height 0.5s"}, 100)
+    window.setTimeout(() => {
+        planetDropdownBox.style.transition = "height 0.5s";
+    }, 100);
 });
 }
