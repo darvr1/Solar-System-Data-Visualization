8cc7dd16116da0ffbc4d47615c1a3c430633a8cf
student <dalegeethoob@gmail.com>
2023-12-02 15:45:55 -0800

    replaced onclick to switch statement for visualization select category

--- public/script.js
+++ public/script.js
@@ -42,8 +42,45 @@ window.addEventListener("load", () => {
 }
 
 { // Plotly
-function bubbleChart() {
-    var trace1 = {
+const select = document.getElementById("category");
+function selectCategory() {
+    switch (select.value) {
+        case "Mass":
+            massData();
+            break;
+        case "Diameter":
+            diameterData();
+            break;
+        case "Density":
+            densityData();
+            break;
+        case "Surface":
+            surfaceData();
+            break;
+        case "Escape":
+            escapeData();
+            break;
+        case "Rotation":
+            rotationData();
+            break;
+        case "Length":
+            lengthData();
+            break;
+        case "Distance":
+            distanceData();
+            break;
+    }
+}
+
+// Keep plotly loaded when refreshed if in visualization page
+if (window.location.href == "http://localhost:3000/visualization.html") {
+    window.addEventListener("load", () => {
+        selectCategory();
+    });
+}
+
+function massData() {
+    let trace1 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [8.2, 27.4, 28.1, 8.3, 100, 41.4, 29.4, 30.1],
         text: ['Mass: 0.33', 'Mass: 4.87', 'Mass: 5.97', 'Mass: 0.642', 'Mass: 1898', 'Mass: 568', 'Mass: 86.8', 'Mass: 102'],
@@ -54,9 +91,9 @@ function bubbleChart() {
         }
     };
 
-    var data = [trace1];
+    let data = [trace1];
 
-    var layout = {
+    let layout = {
         title: 'Mass (10^24kg)',
         showlegend: false,
         height: 700,
@@ -66,8 +103,8 @@ function bubbleChart() {
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function bubbleChart1() {
-    var trace2 = {
+function diameterData() {
+    let trace2 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [3.2, 8, 8.5, 4.5, 100, 84, 35.6, 34.4],
         text: ['Diameter: 4879', 'Diameter: 12104', 'Diameter: 12756', 'Diameter: 6792', 'Diameter: 142984','Diameter: 120536', 'Diameter: 51118', 'Diameter: 49528'],
@@ -78,9 +115,9 @@ function bubbleChart1() {
         }
     };
 
-    var data = [trace2];
+    let data = [trace2];
 
-    var layout = {
+    let layout = {
         title: 'Diameter (km)',
         showlegend: false,
         height: 700,
@@ -90,8 +127,8 @@ function bubbleChart1() {
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function bubbleChart2() {
-    var trace3 = {
+function densityData() {
+    let trace3 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [89.9, 80.1, 100, 61.8, 26, 10, 25.4, 47.2],
         text: ['Density: 5429', 'Density: 5243', 'Density: 5514', 'Density: 3934', 'Density: 1326', 'Density: 687', 'Density: 1270', 'Density: 1638'],
@@ -102,9 +139,9 @@ function bubbleChart2() {
         }
     };
 
-    var data = [trace3];
+    let data = [trace3];
 
-    var layout = {
+    let layout = {
         title: 'Density (kg/m^3)',
         showlegend: false,
         height: 700,
@@ -115,8 +152,8 @@ function bubbleChart2() {
     
 }
 
-function bubbleChart3() {
-    var trace4 = {
+function surfaceData() {
+    let trace4 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [18.5, 44.5, 49, 18.5, 115.5, 45, 43.5, 55],
         text: ['Surface Gravity: 3.7', 'Surface Gravity: 8.9', 'Surface Gravity: 9.8', 'Surface Gravity: 3.7', 'Surface Gravity: 23.1', 'Surface Gravity: 9', 'Surface Gravity: 8.7', 'Surface Gravity: 11'],
@@ -127,9 +164,9 @@ function bubbleChart3() {
         }
     };
 
-    var data = [trace4];
+    let data = [trace4];
 
-    var layout = {
+    let layout = {
         title: 'Surface Gravity (m/s^2)',
         showlegend: false,
         height: 700,
@@ -139,8 +176,8 @@ function bubbleChart3() {
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function bubbleChart4() {
-    var trace5 = {
+function escapeData() {
+    let trace5 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [4.3, 10.4, 11.2, 5, 59.5, 35.5, 21.3, 23.5],
         text: ['Escape Velocity: 4.3', 'Escape Velocity: 10.4', 'Escape Velocity: 11.2', 'Escape Velocity: 5', 'Escape Velocity: 59.5', 'Escape Velocity: 35.5', 'Escape Velocity: 21.3', 'Escape Velocity: 23.5'],
@@ -151,9 +188,9 @@ function bubbleChart4() {
         }
     };
 
-    var data = [trace5];
+    let data = [trace5];
 
-    var layout = {
+    let layout = {
         title: 'Escape Velocity (km/s)',
         showlegend: false,
         height: 700,
@@ -163,8 +200,8 @@ function bubbleChart4() {
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function bubbleChart5() {
-    var trace6 = {
+function rotationData() {
+    let trace6 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [96.1, 10, 48.8, 49.6, 48, 48.1, 48.6, 47.8],
         text: ['Rotation Period: 1407.6', 'Rotation Period: -5832.5', 'Rotation Period: 23.9', 'Rotation Period: 24.6', 'Rotation Period: 9.9', 'Rotation Period: 10.7', 'Rotation Period: -17.2', 'Rotation Period: 16.1'],
@@ -175,9 +212,9 @@ function bubbleChart5() {
         }
     };
 
-    var data = [trace6];
+    let data = [trace6];
 
-    var layout = {
+    let layout = {
         title: 'Rotaion Period (hours)',
         showlegend: false,
         height: 700,
@@ -188,8 +225,8 @@ function bubbleChart5() {
     
 }
 
-function bubbleChart6() {
-    var trace7 = {
+function lengthData() {
+    let trace7 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [208.2, 127.8, 9, 10.2, 3.6, 6.3, 148.5, 106.2],
         text: ['Length of Day: 4222.6', 'Length of Day: 2802', 'Length of Day: 24', 'Length of Day: 24.7', 'Length of Day: 9.9', 'Length of Day: 10.7', 'Length of Day: 17.2', 'Length of Day: 16.1'],
@@ -200,9 +237,9 @@ function bubbleChart6() {
         }
     };
 
-    var data = [trace7];
+    let data = [trace7];
 
-    var layout = {
+    let layout = {
         title: 'Length of Day (hours)',
         showlegend: false,
         height: 700,
@@ -212,8 +249,8 @@ function bubbleChart6() {
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function bubbleChart7() {
-    var trace8 = {
+function distanceData() {
+    let trace8 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [17.2, 33.6, 50, 74.7, 99.8, 134.7, 192.6, 235.1],
         text: ['Distance from Sun: 57.9', 'Distance from Sun: 108.2', 'Distance from Sun: 149.6', 'Distance from Sun: 228', 'Distance from Sun: 778.5', 'Distance from Sun: 1432', 'Distance from Sun: 2867', 'Distance from Sun: 4515'],
@@ -224,9 +261,9 @@ function bubbleChart7() {
         }
     };
 
-    var data = [trace8];
+    let data = [trace8];
 
-    var layout = {
+    let layout = {
         title: 'Distance from Sun (10^6 km)',
         showlegend: false,
         height: 700,
@@ -235,7 +272,4 @@ function bubbleChart7() {
 
     Plotly.newPlot('myDiv', data, layout);
 }
-
-
-
 }
