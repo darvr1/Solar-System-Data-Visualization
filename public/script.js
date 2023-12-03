9d6ce3179d758835f965fac99d022f45488e51ee
student <dalegeethoob@gmail.com>
2023-12-03 13:36:03 -0800

    added text background and home animation

--- public/script.js
+++ public/script.js
@@ -42,32 +42,38 @@ window.addEventListener("load", () => {
 }
 
 { // Plotly
-const select = document.getElementById("category");
+const categoryselect = document.getElementById("category");
+const typeselect = document.getElementById("type");
+
+function selectType() {
+    return typeselect.value;
+}
+    
 function selectCategory() {
-    switch (select.value) {
+    switch (categoryselect.value) {
         case "Mass":
-            massData();
+            massData(selectType());
             break;
         case "Diameter":
-            diameterData();
+            diameterData(selectType());
             break;
         case "Density":
-            densityData();
+            densityData(selectType());
             break;
         case "Surface":
-            surfaceData();
+            surfaceData(selectType());
             break;
         case "Escape":
-            escapeData();
+            escapeData(selectType());
             break;
         case "Rotation":
-            rotationData();
+            rotationData(selectType());
             break;
         case "Length":
-            lengthData();
+            lengthData(selectType());
             break;
         case "Distance":
-            distanceData();
+            distanceData(selectType());
             break;
     }
 }
@@ -79,10 +85,11 @@ if (window.location.href == "http://localhost:3000/visualization.html") {
     });
 }
 
-function massData() {
+function massData(type) {
     let trace1 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [8.2, 27.4, 28.1, 8.3, 100, 41.4, 29.4, 30.1],
+        type: type,
         text: ['Mass: 0.33', 'Mass: 4.87', 'Mass: 5.97', 'Mass: 0.642', 'Mass: 1898', 'Mass: 568', 'Mass: 86.8', 'Mass: 102'],
         mode: 'markers',
         marker: {
@@ -97,16 +104,17 @@ function massData() {
         title: 'Mass (10^24kg)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function diameterData() {
+function diameterData(type) {
     let trace2 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [3.2, 8, 8.5, 4.5, 100, 84, 35.6, 34.4],
+        type: type,
         text: ['Diameter: 4879', 'Diameter: 12104', 'Diameter: 12756', 'Diameter: 6792', 'Diameter: 142984','Diameter: 120536', 'Diameter: 51118', 'Diameter: 49528'],
         mode: 'markers',
         marker: {
@@ -121,16 +129,17 @@ function diameterData() {
         title: 'Diameter (km)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function densityData() {
+function densityData(type) {
     let trace3 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [89.9, 80.1, 100, 61.8, 26, 10, 25.4, 47.2],
+        type: type,
         text: ['Density: 5429', 'Density: 5243', 'Density: 5514', 'Density: 3934', 'Density: 1326', 'Density: 687', 'Density: 1270', 'Density: 1638'],
         mode: 'markers',
         marker: {
@@ -145,17 +154,18 @@ function densityData() {
         title: 'Density (kg/m^3)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
     
 }
 
-function surfaceData() {
+function surfaceData(type) {
     let trace4 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [18.5, 44.5, 49, 18.5, 115.5, 45, 43.5, 55],
+        type: type,
         text: ['Surface Gravity: 3.7', 'Surface Gravity: 8.9', 'Surface Gravity: 9.8', 'Surface Gravity: 3.7', 'Surface Gravity: 23.1', 'Surface Gravity: 9', 'Surface Gravity: 8.7', 'Surface Gravity: 11'],
         mode: 'markers',
         marker: {
@@ -170,16 +180,17 @@ function surfaceData() {
         title: 'Surface Gravity (m/s^2)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function escapeData() {
+function escapeData(type) {
     let trace5 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [4.3, 10.4, 11.2, 5, 59.5, 35.5, 21.3, 23.5],
+        type: type,
         text: ['Escape Velocity: 4.3', 'Escape Velocity: 10.4', 'Escape Velocity: 11.2', 'Escape Velocity: 5', 'Escape Velocity: 59.5', 'Escape Velocity: 35.5', 'Escape Velocity: 21.3', 'Escape Velocity: 23.5'],
         mode: 'markers',
         marker: {
@@ -194,16 +205,17 @@ function escapeData() {
         title: 'Escape Velocity (km/s)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function rotationData() {
+function rotationData(type) {
     let trace6 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [96.1, 10, 48.8, 49.6, 48, 48.1, 48.6, 47.8],
+        type: type,
         text: ['Rotation Period: 1407.6', 'Rotation Period: -5832.5', 'Rotation Period: 23.9', 'Rotation Period: 24.6', 'Rotation Period: 9.9', 'Rotation Period: 10.7', 'Rotation Period: -17.2', 'Rotation Period: 16.1'],
         mode: 'markers',
         marker: {
@@ -218,17 +230,18 @@ function rotationData() {
         title: 'Rotaion Period (hours)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
     
 }
 
-function lengthData() {
+function lengthData(type) {
     let trace7 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [208.2, 127.8, 9, 10.2, 3.6, 6.3, 148.5, 106.2],
+        type: type,
         text: ['Length of Day: 4222.6', 'Length of Day: 2802', 'Length of Day: 24', 'Length of Day: 24.7', 'Length of Day: 9.9', 'Length of Day: 10.7', 'Length of Day: 17.2', 'Length of Day: 16.1'],
         mode: 'markers',
         marker: {
@@ -243,16 +256,17 @@ function lengthData() {
         title: 'Length of Day (hours)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
 }
 
-function distanceData() {
+function distanceData(type) {
     let trace8 = {
         x: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
         y: [17.2, 33.6, 50, 74.7, 99.8, 134.7, 192.6, 235.1],
+        type: type,
         text: ['Distance from Sun: 57.9', 'Distance from Sun: 108.2', 'Distance from Sun: 149.6', 'Distance from Sun: 228', 'Distance from Sun: 778.5', 'Distance from Sun: 1432', 'Distance from Sun: 2867', 'Distance from Sun: 4515'],
         mode: 'markers',
         marker: {
@@ -267,7 +281,7 @@ function distanceData() {
         title: 'Distance from Sun (10^6 km)',
         showlegend: false,
         height: 700,
-        width: 1200
+        width: 950
     };
 
     Plotly.newPlot('myDiv', data, layout);
